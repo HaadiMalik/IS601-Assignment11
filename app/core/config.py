4d58fbd60721d5e7bdedff0a2dfb0e838ebb2dd9
HaadiMalik <frantic438@gmail.com>
2025-11-16 17:56:19 -0500

    Reorganize app/core files

--- app/core/config.py
+++ app/core/config.py
@@ -1,9 +1,11 @@
 from pydantic_settings import BaseSettings
+from typing import Optional
 
 class Settings(BaseSettings):
     DATABASE_URL: str = "postgresql://postgres:postgres@localhost:5432/fastapi_db"
     
     class Config:
         env_file = ".env"
+        case_sensitive = True
 
 settings = Settings()
